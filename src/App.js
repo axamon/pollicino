8872ad667f788ed1991c427dc27be74835289796
00246506 <alberto.bregliano@telecomitalia.it>
2022-04-28 23:30:23 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import { Lavori } from './components/Lavori'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
+  <>
+    <h1 id="title">Lavori in corso...</h1>
     <Lavori />
+    <footer>
+      <p><small>&copy; 2022 Pollicino Green</small></p>
+    </footer>
+  </>
   );
 }
 
