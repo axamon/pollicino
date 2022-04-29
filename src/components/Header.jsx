104d03c025de34ce454ed2f743d4d22bc0fb006e
00246506 <alberto.bregliano@telecomitalia.it>
2022-04-29 15:10:55 +0200

    starticon ok

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ export default class Header extends Component {
         <nav>
             <ul>
                 <li><a href="#">Home</a></li>
-                <li><a href="#">Chi siamo</a></li>
+                <li><a href="#">ChiSiamo</a></li>
                 <li><a href="#">Contatti</a></li>
             </ul>
         </nav>
--- src/components/Occhio.jsx
+++ src/components/Occhio.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
-import occhio from './eye4.jpg'
+import occhio from './blueeye.png'
 import './occhio.css'
+import starticon from './starticon.webp'
 
 export class Occhio extends Component {
   render() {
     return (
-      <div id="occhio"><img width="60%" src={occhio} alt='occhio' />
+      <>
+     
+      <div className="occhio">
+        <a id='starticon' href='http://www.openbsd.org'><img  width="10%" src={starticon} alt='starticon' /></a>
+        <img width="60%" src={occhio} alt='occhio' />
       </div>
+      
+      </>
     )
   }
 }
