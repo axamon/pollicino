ebd5d9806c4b751c4304fbae87f44921055ed6a9
00246506 <alberto.bregliano@telecomitalia.it>
2022-04-30 14:16:47 +0200

    ok

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ export default class Header extends Component {
         <nav>
             <ul>
                 <li><a href="#">Home</a></li>
-                <li><a href="#">ChiSiamo</a></li>
+                <li><a href="#">Chi Siamo</a></li>
                 <li><a href="#">Contatti</a></li>
             </ul>
         </nav>
